d35ef203a9d470432304999a680e9bd1372168a4
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-01-22 16:12:07 +0100

    - ADD dynamic elements, taken from the object obtained from the API response

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,25 +1,44 @@
 <script>
+    import SingleCard from '../components/SingleCard.vue';
+    import { store } from '../store';
+    
     export default {
         data() {
             return { 
-                 
+
+                store
             }
         },
+        components: {
+            SingleCard,
+        }, 
     }
     </script>
 
 <template>
     <div class="bg-warning p-5 ">
-        <select name="" id="">
-            <option value="">Alien</option>
-        </select>
-
-        <div class="row p-5 bg-white">
-            <div class="col-12 p-3 bg-dark text-white fw-bold">
-                <span>Found # cards</span>
-            </div>
-
+        <div class=" container-xl ">
+            <div class=" w-25 mb-3">
+                <select class="form-select" aria-label="Default select example">
+                    <option value="1">Alien</option>
+                </select>
+            </div>  
             
+
+            <div class="row p-5 bg-white g-0 ">
+                <div class="col-12 p-3 bg-dark text-white fw-bold mb-2 rounded-1  ">
+                    <span>Found {{ store.cardsList.length }} cards</span>
+                </div>
+
+                <SingleCard 
+                v-for="(elem, i) in store.cardsList"
+                :card="elem"
+                :arc="elem.archetype"
+                />
+
+                
+                
+            </div>
         </div>
     </div>
 </template>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,65 @@
+<script>
+    
+    export default {
+        data() {
+            return { 
+                 
+            }
+        },
+        props: {
+            card: Object,
+            arc: {
+                type: String,
+                default: 'Not Found',
+            },
+        },
+    }
+    </script>
+
+<template>
+    <div class="card-col mb-4">
+        <div class="card_frame">
+            <img :src="card.card_images[0].image_url" :alt="card.name" class="w-100">
+        </div>
+
+        <div class="card_info text-center px-2 py-4 bg-warning">
+            <h3 class="">
+                {{ card.name }}
+            </h3>
+
+            <span>
+                {{ arc }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.card-col{
+    width: calc(100% / 5);
+    padding: 0 30px;
+    .card_frame{
+    width: 100%;
+    }
+    .card_info{
+    height: 200px;
+
+        h3{
+        font-size: 1.35rem;
+        color: white;
+        }
+
+        span{
+
+        font-size: 1.15rem;
+
+        }
+
+    }
+}
+
+    
+    
+    
+</style>
