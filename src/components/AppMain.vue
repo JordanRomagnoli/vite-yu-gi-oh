5e4a2248439e6cad6646b76e079a167f5077fcb8
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-01-23 17:20:06 +0100

    - ADD component for count visible card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,5 @@
 <script>
+    import CardCounter from './CardCounter.vue';
     import SingleCard from '../components/SingleCard.vue';
     import { store } from '../store';
     
@@ -10,13 +11,10 @@
             }
         },
         components: {
+            CardCounter,
             SingleCard,
         },
-        methods: {
-            changeActiveArc(newArc){
-                this.store.activeArc = newArc;
-            }
-        },
+        
     }
     </script>
 
@@ -24,18 +22,15 @@
     <div class="bg-warning p-5 ">
         <div class=" container-xl ">
             <div class=" w-25 mb-3">
-                <select class="form-select" name="type" v-model="store.activeArc" @click="$emit('arcFilter')" aria-label="Default select example">
+                <select class="form-select" name="type" v-model="store.activeArc" @change="$emit('arcFilter')" aria-label="Default select example">
                     <option value="">All</option>
-                    <option name="type" v-for="(elem, i) in store.arcList" :value="elem" @click="changeActiveArc(value)" :key="i">{{elem}}</option>
+                    <option name="type" v-for="(elem, i) in store.arcList" :value="elem" :key="i">{{elem}}</option>
                 </select>
             </div>  
             
 
             <div class="row p-5 bg-white g-0 rounded-4">
-                <div class="col-12 p-3 bg-dark text-white fw-bold mb-2 rounded-1 ">
-                    <span>Found {{ this.store.cardsList.length }} cards</span>
-                </div>
-
+                <CardCounter/>
             
                 <SingleCard 
                 v-for="(elem, i) in this.store.cardsList"
---
+++ src/components/CardCounter.vue
@@ -0,0 +1,22 @@
+<script>
+    import {store} from '../store';
+
+    export default {
+        data() {
+            return { 
+
+                store,
+            }
+        },
+    }
+    </script>
+
+<template>
+    <div class="col-12 p-3 bg-dark text-white fw-bold mb-2 rounded-1 ">
+        <span>Found {{ this.store.cardsList.length }} cards</span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
